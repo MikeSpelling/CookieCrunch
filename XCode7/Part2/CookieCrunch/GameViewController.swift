b170adef45a1f0802bb6fff7ea6b4bcebf91542a
MikeSpelling <mike.spelling@hotmail.co.uk>
2016-02-20 15:45:29 +0000

    Add 2 parts with notes

---
+++ XCode7/Part2/CookieCrunch/GameViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    var scene: GameScene!
+    var level: Level!
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.AllButUpsideDown
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        level = Level(filename: "Level_1")
+        scene.level = level
+        scene.addTiles()
+        
+        // Present the scene.
+        skView.presentScene(scene)
+        
+        beginGame()
+    }
+    
+    func beginGame() {
+        shuffle()
+    }
+    
+    func shuffle() {
+        let newCookies = level.shuffle()
+        scene.addSpritesForCookies(newCookies)
+    }
+    
+}
---
+++ XCode7/Part2/CookieCrunch/Level.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+let NumColumns = 9
+let NumRows = 9
+
+class Level {
+    private var cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)
+    private var tiles = Array2D<Tile>(columns: NumColumns, rows: NumRows)
+    
+    init(filename: String) {
+        // 1
+        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename) {
+            // 2
+            if let tilesArray: AnyObject = dictionary["tiles"] {
+                // 3
+                for (row, rowArray) in (tilesArray as! [[Int]]).enumerate() {
+                    // 4
+                    let tileRow = NumRows - row - 1
+                    // 5
+                    for (column, value) in rowArray.enumerate() {
+                        if value == 1 {
+                            tiles[column, tileRow] = Tile()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func cookieAtColumn(column: Int, row: Int) -> Cookie? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return cookies[column, row]
+    }
+    
+    func shuffle() -> Set<Cookie> {
+        return createInitialCookies()
+    }
+    
+    private func createInitialCookies() -> Set<Cookie> {
+        var set = Set<Cookie>()
+        
+        // 1
+        for row in 0..<NumRows {
+            for column in 0..<NumColumns {
+                
+                // This line is new
+                if tiles[column, row] != nil {
+                    // 2
+                    var cookieType = CookieType.random()
+                    
+                    // 3
+                    let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                    cookies[column, row] = cookie
+                    
+                    // 4
+                    set.insert(cookie)
+                }
+            }
+        }
+        return set
+    }
+    
+    func tileAtColumn(column: Int, row: Int) -> Tile? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return tiles[column, row]
+    }
+    
+}
